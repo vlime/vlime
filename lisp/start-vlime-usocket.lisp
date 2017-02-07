95ad442e4f8432ff7a982cc57346fa188ee39dde
Kay Z <l04m33@gmail.com>
2017-02-08 00:01:35 +0800

    Normalize ASDF package names

--- lisp/start-vlime-usocket.lisp
+++ lisp/start-vlime-usocket.lisp
@@ -3,11 +3,11 @@
 (defparameter *vlime-home* (make-pathname :directory (pathname-directory *load-truename*)))
 
 (defun load-vlime ()
-  (asdf/find-system:initialize-source-registry
+  (asdf:initialize-source-registry
     `(:source-registry
        (:directory ,*vlime-home*)
        :inherit-configuration))
-  (asdf/operate:load-system :vlime-usocket))
+  (asdf:load-system :vlime-usocket))
 
 (load-vlime)
 (swank:create-server :port 4005 :style :spawn :dont-close t)
--- lisp/start-vlime.lisp
+++ lisp/start-vlime.lisp
@@ -3,11 +3,11 @@
 (defparameter *vlime-home* (make-pathname :directory (pathname-directory *load-truename*)))
 
 (defun load-vlime ()
-  (asdf/find-system:initialize-source-registry
+  (asdf:initialize-source-registry
     `(:source-registry
        (:directory ,*vlime-home*)
        :inherit-configuration))
-  (asdf/operate:load-system :vlime))
+  (asdf:load-system :vlime))
 
 (load-vlime)
 (swank:create-server :port 4005 :style :spawn :dont-close t)
