42c25a735c39b1935db156800abf60841ddc7744
Philipp Marek <philipp@marek.priv.at>
2020-11-13 16:24:37 +0100

    Now all the server parts can be dropped.
    Keeping only the defering-to-swank-parts in.

--- lisp/src/vlime.lisp
+++ lisp/src/vlime.lisp
@@ -8,9 +8,6 @@
 (in-package #:vlime)
 
 
-(defgeneric start-server (backend host port swank-host swank-port dont-close))
-
-
 (define-condition quicklisp-not-found-error (error)
   ((package :initarg :package
             :initform nil
@@ -45,66 +42,11 @@
                   (swank-interface #(127 0 0 1) swank-interface-p)
                   (swank-port 0 swank-port-p)
                   (dont-close t))
-  (when (not backend)
-    (let ((preferred-style (dyn-call "SWANK/BACKEND" "PREFERRED-COMMUNICATION-STYLE")))
-      (case preferred-style
-        (:spawn
-          (setf backend :vlime-usocket))
-        (:fd-handler
-          (setf backend :vlime-sbcl))
-        ((nil)
-         (setf backend :vlime-patched))
-        (t
-          (format *error-output*
-                  "Vlime: Communication style ~s not supported.~%" preferred-style)
-          (return-from main)))))
-
-  (let ((swank-comm-style
-          (dyn-call "SWANK/BACKEND" "PREFERRED-COMMUNICATION-STYLE")))
-    (labels ((announce-swank-port (port)
-               (setf swank-port port))
-             (announce-vlime-port (port)
-               (when port-file
-                 (with-open-file (pf port-file
-                                  :direction :output
-                                  :if-exists :supersede
-                                  :if-does-not-exist :create)
-                   (with-standard-io-syntax
-                     (write port :stream pf)))))
-             (start-vlime-server (backend)
-             (announce-vlime-port swank-port))
-             (start-swank-server (announce-port)
-               (let ((swank-loopback (symbol-value (find-symbol "*LOOPBACK-INTERFACE*" "SWANK"))))
-                 ; This is... ugly and not safe at all, but we don't have access
-                 ; to the SWANK package when bootstrapping.
-                 (unwind-protect
-                   (progn
-                     (setf (symbol-value (find-symbol "*LOOPBACK-INTERFACE*" "SWANK"))
-                           (format nil "~{~a~^.~}"
-                                   (loop for b across swank-interface collect b)))
-                     (dyn-call "SWANK" "SETUP-SERVER"
-                               swank-port announce-port swank-comm-style dont-close nil))
-                   (setf (symbol-value (find-symbol "*LOOPBACK-INTERFACE*" "SWANK")) swank-loopback)))))
-      (ecase backend
-        (:vlime-usocket
-          (try-to-load :vlime-usocket)
-          (when start-swank
-            (start-swank-server #'announce-swank-port))
-          (start-vlime-server :usocket))
-        (:vlime-sbcl
-          (try-to-load :vlime-sbcl)
-          (when start-swank
-            (start-swank-server #'announce-swank-port))
-          (start-vlime-server :sbcl))
-        (:vlime-patched
-          (try-to-load :vlime-patched)
-          (dyn-call "VLIME-PATCHED" "PATCH-SWANK")
-          ;; SWANK-INTERFACE and SWANK-PORT are ignored in this case.
-          (when (or swank-interface-p swank-port-p)
-            (warn "SWANK-INTERFACE and SWANK-PORT are ignored when using the VLIME-PATCHED backend."))
-          (setf swank-interface interface
-                swank-port port)
-          (start-swank-server
+  (declare (ignore backend interface port port-file start-swank swank-interface-p swank-port-p))
+  (dyn-call "SWANK" "SETUP-SERVER"
+            swank-port 
             #'(lambda (port)
-                (format t "Server created: (~a ~a)~%" swank-interface port)
-                (announce-vlime-port port))))))))
+                (format t "Server created: (~a ~a)~%" swank-interface port))
+            swank:*communication-style*
+            dont-close 
+            nil))
